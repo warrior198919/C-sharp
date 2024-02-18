7289eaa89d52d6478edcae031ff202881caff31d
Aleksey Graunov <agraunov@gmail.com>
2024-02-18 23:48:02 +0300

    ДЗ из четырех задач

--- Lesson02/Task01/Program.cs
+++ Lesson02/Task01/Program.cs
@@ -1,2 +1,45 @@
 ﻿// // See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
+using System;
+
+public class Answer
+{
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine($"{firstNumber} больше чем {secondNumber}");
+            Console.WriteLine($"{secondNumber} меньше чем {firstNumber}");
+        }
+        else if (firstNumber < secondNumber)
+        {
+            Console.WriteLine($"{firstNumber} меньше чем {secondNumber}");
+            Console.WriteLine($"{secondNumber} больше чем {firstNumber}");
+        }
+        else
+        {
+            Console.WriteLine($"{firstNumber} равно {secondNumber}");
+        }
+    }
+
+    // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args)
+    {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2)
+        {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 5;
+            secondNumber = 5;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
