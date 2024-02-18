1389d092de31bf665a654775edf4f244b5a394b9
Aleksey Graunov <agraunov@gmail.com>
2024-02-19 00:48:34 +0300

    Выполнение ДЗ N3

---
+++ Lesson04/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+class Answer {
+    static void CheckIfEven(int number)
+    {
+      // Введите свое решение ниже
+        Console.WriteLine(number % 2 == 0 ? "четное" : "нечетное");
+
+    }
+  
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 6;
+        }
+
+        // Не удаляйте строки ниже
+        CheckIfEven(number);
+    }
+}
