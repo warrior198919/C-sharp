2791c17d7b9f808d00c669c4a7377a7a0615958e
Aleksey Graunov <agraunov@gmail.com>
2024-02-20 00:32:47 +0300

    new error input

--- Lesson01/Task4/Program.cs
+++ Lesson01/Task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Лекции по програмированнию гири
+int a = 6;
+int b = 7;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+if (b > max)
+{
+    max = b;
+}}
