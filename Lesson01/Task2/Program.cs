b08e48622db3c9671fd901cd41b8d3ced0e5642b
Aleksey Graunov <agraunov@gmail.com>
2024-02-19 18:21:50 +0300

    продолжаем изучать C#

--- Lesson01/Task2/Program.cs
+++ Lesson01/Task2/Program.cs
@@ -1,8 +1,11 @@
-﻿int i1 = 5;
+﻿using Internal;
+
+int i1 = 5;
 int i2 = 6;
 int sum = i1 + i2;
 Console.WriteLine(sum);
 
+//сложение переменных
 long l1 = 6;
 long sum2 = l1 + i1;
 Console.WriteLine(sum2);
@@ -10,9 +13,14 @@ Console.WriteLine(sum2);
 double d1 = 5.5;
 double sum3 = d1 + i1;
 
+//умноженние переменных
 int product1 = i1 * i2;
 long product2 = l1 * i1;
 double product3 = i1 * d1;
 Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
+
+//деление переменных
+int quotient = i1 / i2;
+Console.WriteLine(quotient);
