cdc128c5e2967da54fbf6bbea8e9548994cb4ffb
Aleksey Graunov <agraunov@gmail.com>
2024-02-20 01:32:03 +0300

    add error cs0201

--- Lesson01/Task4/Program.cs
+++ Lesson01/Task4/Program.cs
@@ -7,3 +7,20 @@ int e = 10;
 
 int max = a;
 
+if (b > max)
+{
+    max + b;
+}
+if (c > max)
+{
+    max + c;
+}
+if (d > max)
+{
+    max + d;
+}
+if (e> max)
+{
+    max + e;
+}
+Console.WriteLine(max);
