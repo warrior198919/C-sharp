63871c8e41e26a56be027aa6d9d37a87c9ad95c6
Aleksey Graunov <agraunov@gmail.com>
2024-02-20 02:25:02 +0300

    в int целочисленное /10/ выдает оштбку

--- Lesson01/Task4/Program.cs
+++ Lesson01/Task4/Program.cs
@@ -1,27 +1,26 @@
-﻿//Лекции по програмированнию гири
-//Console.WriteLine("Hello world");
-int a = 6;
+﻿int a = 6;
 int b = 7;
 int c = 9;
 int d = 4;
-int e = 10;
+//int e = 10;
 
 int max = a;
 
 if (b > max)
 {
-    max + b;
+    max = b;
 }
 if (c > max)
 {
-    max + c;
+    max = c;
 }
 if (d > max)
 {
-    max + d;
-}
-if (e > max)
-{
-    max + e;
+    max = d;
 }
 Console.WriteLine(max);
+
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
