21be114ced4bd5752dbe8640a21f3fc1f6942dea
Aleksey Graunov <agraunov@gmail.com>
2024-02-20 05:12:53 +0300

    add Задание2 Координаты принимает на вход координаты точки (X и Y)

---
+++ Lesson06/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите координату X: ");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите координату Y: ");
+        double y = double.Parse(Console.ReadLine());
+
+        if (x != 0 && y != 0)
+        {
+            int quadrant;
+
+            if (x > 0 && y > 0)
+            {
+                quadrant = 1;
+            }
+            else if (x < 0 && y > 0)
+            {
+                quadrant = 2;
+            }
+            else if (x < 0 && y < 0)
+            {
+                quadrant = 3;
+            }
+            else
+            {
+                quadrant = 4;
+            }
+
+            Console.WriteLine($"Точка находится в {quadrant}-й координатной четверти.");
+        }
+        else
+        {
+            Console.WriteLine("Введенные координаты не соответствуют условию (X ≠ 0 и Y ≠ 0).");
+        }
+    }
+}
