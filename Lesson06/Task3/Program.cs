16022a91e965dcc15e34dbef61446c47e1ac823d
Aleksey Graunov <agraunov@gmail.com>
2024-02-20 05:22:53 +0300

    add Задание3 принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа

---
+++ Lesson06/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите целое число из отрезка [10, 99]: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number >= 10 && number <= 99)
+        {
+            int firstDigit = number / 10;
+            int secondDigit = number % 10;
+
+            int maxDigit = (firstDigit > secondDigit) ? firstDigit : secondDigit;
+
+            Console.WriteLine($"Наибольшая цифра числа {number}: {maxDigit}");
+        }
+        else
+        {
+            Console.WriteLine("Введенное число не соответствует диапазону [10, 99].");
+        }
+    }
+}
