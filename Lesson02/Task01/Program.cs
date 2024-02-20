748c2d2303e8ada1899f6f3c363761b96f803bc9
Aleksey Graunov <agraunov@gmail.com>
2024-02-20 04:25:25 +0300

    выполнение ДЗ Задача1

--- Lesson02/Task01/Program.cs
+++ Lesson02/Task01/Program.cs
@@ -3,28 +3,26 @@
 // Первая Задача
 using System;
 
-public class Answer
-{
+public class Answer {
     static void CompareNumbers(int firstNumber, int secondNumber)
     {
-        string result = (firstNumber > secondNumber) ? "больше" : (firstNumber < secondNumber) ? "меньше" : "равно";
-        Console.WriteLine($"{firstNumber} {result} чем {secondNumber}");
+        Console.WriteLine(firstNumber > secondNumber ? "первое число больше" : firstNumber < secondNumber ? "второе число больше" : "числа равны");
     }
 
+    //if (firstNuber == secondNumber)
+    // {
+    //    Console.WriteLine($"числа равны");
+    // }
     // Не удаляйте и не меняйте метод Main! 
-    static public void Main(string[] args)
-    {
+    static public void Main(string[] args) {
         int firstNumber, secondNumber;
 
-        if (args.Length >= 2)
-        {
+        if (args.Length >= 2) {
             firstNumber = int.Parse(args[0]);
             secondNumber = int.Parse(args[1]);
-        }
-        else
-        {
-            // Здесь вы можете поменять значения для отправки кода на Выполнение
-            firstNumber = 5;
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 6;
             secondNumber = 6;
         }
 
