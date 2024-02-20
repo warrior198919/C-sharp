15bd266f8bb6035212c8740a613a63c8bdcb1295
Aleksey Graunov <agraunov@gmail.com>
2024-02-20 05:40:29 +0300

    add Задача4 на вход принимает натуральное число N, а на выходе показывает его цифры через запятую

--- Lesson06/Task4/Program.cs
+++ Lesson06/Task4/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите натуральное число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n > 0)
+        {
+            Console.Write($"Цифры числа {n} через запятую: ");
+
+            // Преобразование числа в строку для посимвольного доступа
+            string numberAsString = n.ToString();
+
+            // Вывод цифр через запятую
+            for (int i = 0; i < numberAsString.Length; i++)
+            {
+                Console.Write(numberAsString[i]);
+                if (i < numberAsString.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Введенное число не является натуральным.");
+        }
+    }
+}
