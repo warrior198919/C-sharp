8e7401e4a0b0f2d56d46e1165e983b8f7f8472b9
Aleksey Graunov <agraunov@gmail.com>
2024-02-20 04:45:43 +0300

    add Задача1

--- Lesson06/Task1/Program.cs
+++ Lesson06/Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    static void Main() {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isMultipleOf7And23 = (number % 7 == 0) && (number % 23 == 0);
+
+        if (isMultipleOf7And23) {
+            Console.WriteLine($"{number} кратно и 7, и 23.");
+        } else {
+            Console.WriteLine($"{number} не кратно одновременно 7 и 23.");
+        }
+    }
+}
