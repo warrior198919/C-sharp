9d1fdc8357796f03d64fa3ca171fd06cff507d5b
Aleksey Graunov <agraunov@gmail.com>
2024-02-20 14:33:39 +0300

    Пример задачи1 ИДЕАЬНЫЙ ДЗ простые алг.

---
+++ Lesson01/Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 7 == 0 && a % 23 == 0)
+        {
+            Console.WriteLine("да");
+        }
+        else
+        {
+            Console.WriteLine("нет");
+        }
+    }
+}
