63bf88c355fba3c092f083ce8df6b01189505976
Aleksey Graunov <agraunov@gmail.com>
2024-02-23 05:34:31 +0300

    NeW Lesson|Massiv repair error WriteLine

--- Lesson01/Task7/Program.cs
+++ Lesson01/Task7/Program.cs
@@ -1,11 +1,11 @@
-﻿int [] arr = new int [5];
+﻿int[] arr = new int[5];
 arr[0] = 5;
 arr[1] = 7;
 arr[2] = 5;
 arr[3] = 4;
 arr[4] = 3;
-Console.Writeline(arr[0]);
-Console.Writeline(arr[1]);
-Console.Writeline(arr[2]);
-Console.Writeline(arr[3]);
-Console.Writeline(arr[4]);
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
