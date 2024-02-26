1589c61b505c45c375cf06fbc9b22cad6089b442
Aleksey Graunov <agraunov@gmail.com>
2024-02-26 03:36:21 +0300

    repair for folder newNass3

--- newNass3/Program.cs
+++ newNass3/Program.cs
@@ -15,7 +15,7 @@ int ArrayToInteger(int[] array)
     return number;
 }
 
-int[] CreateArrayRndInt(int size, int min, int max)clear
+int[] CreateArrayRndInt(int size, int min, int max)
 {
     int[] array = new int[size];
     Random rnd = new Random();
