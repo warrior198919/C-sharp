a246ef0c787eba29238741da203afb63abb0846a
root <root@cograndl.ru>
2024-03-02 11:54:23 +0300

    home wark

--- GPT/Program.cs
+++ GPT/Program.cs
@@ -67,3 +67,174 @@ Cортировка пузырьком
 //     Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
 // }
 // Console.Clear();
+
+
+// Семинары 2 марта 2024 Андрей Булгаков
+// Администратор
+
+// int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+// {
+//     //   0      1
+//     int[,] matrix = new int[rows, columns];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max);
+//         }
+//     }
+
+//     return matrix;
+// }
+
+
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j],5}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void ReplaceEvenIdxElemToSquare(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i += 2)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j += 2)
+//         {
+//             matrix[i, j] *= matrix[i, j];
+//         }
+//     }
+// }
+
+// int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
+// PrintMatrix(array2d);
+
+// Console.WriteLine();
+
+// ReplaceEvenIdxElemToSquare(array2d);
+// PrintMatrix(array2d);
+
+// Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами 
+// (0,0); (1;1) и т.д.
+// Пример
+
+// 2 3 4 3
+// 4 3 4 1
+// 2 9 5 4
+
+// => 2 + 3 + 5 = 10
+
+
+// int[,] CreateMarixRndInt(int rows, int colums, int min, int max)
+// {
+//     int[,] array = new int[rows, colums];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = rnd.Next(min, max);
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j],5}");
+//         }
+//         Console.WriteLine();
+//     }
+
+// }
+
+// int SummMainDiagonal(int[,] matrix)
+// {
+//     int sum = 0;
+//     int min = matrix.GetLength(0);
+//     if (matrix.GetLength(1) < min) min = matrix.GetLength(1);
+//     for (int i = 0; i < min; i++)
+//     {
+//         sum += matrix[i, i];
+//     }
+//     return sum;
+// }
+
+// int[,] matr = CreateMarixRndInt(7, 5, 0, 10);
+
+// PrintMatrix(matr);
+// Console.WriteLine();
+// Console.WriteLine(SummMainDiagonal(matr));
+
+// Shohruh Hakimov
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i], 4}");
+    }
+}
+
+int[,] CreateMarixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] array = new int[rows, colums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+double[] AverageRows(int[,] matrix)
+{
+    double[] array = new double[matrix.GetLength(0)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        double tempAverage = sum / matrix.GetLength(1);
+        array[i] = tempAverage;
+    }
+    return array;
+}
+int[,] matr = CreateMarixRndInt(3, 4, 0, 10);
+PrintMatrix(matr);
+Console.WriteLine();
+double[] mass = AverageRows( matr);
+PrintArray(mass);
