b0feae7db0428fc12c177f00831aedbce9b33543
root <root@cograndl.ru>
2024-03-05 03:05:57 +0300

    repair branch GPT folder

--- GPT/Program.cs
+++ GPT/Program.cs
@@ -1,126 +1,52 @@
-﻿// using System;
-
-// class Пример
-// {
-//     static void Main()
-//     {
-//         // Создаем массив из 10 целых чисел
-//         int[] array = { 2, 5, 2, 3, 6, 3, 7, 8, 2, 4 };
-
-//         // Находим количество четных чисел в массиве
-//         int количествоЧетных = 0;
-//         foreach (int число in array)
-//         {
-//             if (число % 2 == 0)
-//             {
-//                 количествоЧетных++;
-//             }
-//         }
-
-//         // Выводим результат
-//         Console.WriteLine("Количество четных чисел в массиве: " + количествоЧетных);
-//     }
-// }
-// using System;
-// using Internal;
-
-// Console.WriteLine("Введите натуральное число N:");
-// int number = Convert.ToInt32(Console.ReadLine());
-
-// string digits = "";
-// int newNumber = number;
-
-// while (newNumber > 0)
-// {
-//     int digit = newNumber % 10;
-//     digits = digit + (digits == "" ? "" : ",") + digits;
-//     newNumber /= 10;
-// }
-
-// Console.WriteLine($"{number} => {digits}");
-
-/*
-Cортировка пузырьком
-Начальный массив: [3, 1, 5, 0, 7, 9, 8]
-*/
-// Console.WriteLine("Введите кол-во элементов массива: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// int[] array = new int[n];
-// for (int i = 0; i < n; i++)
-// {
-//     Console.Write("Введите значения массива: ");
-//     array[i] = Convert.ToInt32(Console.ReadLine());
-// }
-// Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
-// Console.WriteLine();
-// for (int i = 0; i < n; i++)
-// {
-//     for (int j = 0; j < n - 1; j++)
-//     {
-//         if (array[j] > array[j + 1])
-//         {
-//             int temp = array[j];
-//             array[j] = array[j + 1];
-//             array[j + 1] = temp;
-//         }
-//     }
-//     Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
-// }
-// Console.Clear();
-
-
-// Семинары 2 марта 2024 Андрей Булгаков
+﻿// Семинары 2 марта 2024 Андрей Булгаков
 // Администратор
 
-// int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
-// {
-//     //   0      1
-//     int[,] matrix = new int[rows, columns];
-//     Random rnd = new Random();
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
 
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             matrix[i, j] = rnd.Next(min, max);
-//         }
-//     }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
 
-//     return matrix;
-// }
+    return matrix;
+}
 
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
 
+void ReplaceEvenIdxElemToSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
 
-// void PrintMatrix(int[,] matrix)
-// {
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             Console.Write($"{matrix[i, j],5}");
-//         }
-//         Console.WriteLine();
-//     }
-// }
+int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2d);
 
-// void ReplaceEvenIdxElemToSquare(int[,] matrix)
-// {
-//     for (int i = 0; i < matrix.GetLength(0); i += 2)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j += 2)
-//         {
-//             matrix[i, j] *= matrix[i, j];
-//         }
-//     }
-// }
+Console.WriteLine();
 
-// int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
-// PrintMatrix(array2d);
-
-// Console.WriteLine();
-
-// ReplaceEvenIdxElemToSquare(array2d);
-// PrintMatrix(array2d);
+ReplaceEvenIdxElemToSquare(array2d);
+PrintMatrix(array2d);
 
 // Задайте двумерный массив. Найдите сумму элементов, 
 // находящихся на главной диагонали (с индексами 
