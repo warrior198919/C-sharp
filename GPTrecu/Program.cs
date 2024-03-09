e7cf3c3ba250c58acd825157d4ea3744e91b6597
Aleksey Graunov <agraunov@gmail.com>
2024-03-09 10:32:08 +0300

    Рекурсии

---
+++ GPTrecu/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте значение N Напишите программу которая выведет все натуральные числа 
+//в промежутке от 1 до N
+//Притмер N= => 1 2 3 4 5 
+
+Console.Clear();
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
