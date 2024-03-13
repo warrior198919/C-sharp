28bbb556579d9a8620ee1cdccea4c6ecc9b34fc6
root <root@cograndl.ru>
2024-03-13 19:13:07 +0300

    add lesson01

--- Lesson01/Task1/obj/Debug/net7.0/Task1.AssemblyInfo.cs
+++ Lesson01/Task1/obj/Debug/net7.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9350bce012c15c2b52b4f9554d91cf6f56a3df5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb5052f52cf3de6aef7da71b3a23b73540caac8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
