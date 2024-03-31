a5200798cd284b7284a662406d93e975c363a24b
Aleksey Graunov <agraunov@gmail.com>
2024-03-31 11:47:42 +0300

    синхронизация с git-hub

--- Lesson01/Task1/obj/Debug/net7.0/Task1.AssemblyInfo.cs
+++ Lesson01/Task1/obj/Debug/net7.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28bbb556579d9a8620ee1cdccea4c6ecc9b34fc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
